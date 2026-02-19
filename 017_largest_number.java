2b390900f2b95121b5d72d2c978798feb2b1a2b1
Angelo Zero <angelo.akm@gmail.com>
2026-02-19 23:02:14 +1100

    Largest Number

--- 017_largest_number.java
+++ 017_largest_number.java
@@ -15,6 +15,47 @@
 
 class Solution {
     public String largestNumber(int[] nums) {
+
+        var strAux = "";
+        var intAux = 0;
+        var strAuxFinal = "";
+
+        for(int x = 0; x < nums.length; x++){
+            strAux += String.valueOf(nums[x]);
+        }
         
+        String[] strList = strAux.split("");
+
+        int[] intList = new int[strList.length];
+
+        for(int x = 0; x < strList.length; x++){
+            intList[x] = getValue(strList[x]);
+        }
+
+        for(int x = 0; x < intList.length; x++){
+            for(int z = x + 1; z < intList.length; z++){
+                if(intList[x] < intList[z]){
+                    intAux = intList[x];
+                    intList[x] = intList[z];
+                    intList[z] = intAux;
+                }
+            }
+            strAuxFinal += String.valueOf(intList[x]);
+        }
+
+        for(int x = 0; x < intList.length; x++){
+            System.out.println(strList[x]);
+        }
+
+        return strAuxFinal;
+    }
+
+    private int getValue(String value){
+        try {
+            return Integer.parseInt(value);
+
+        } catch (Exception ex){
+            throw new RuntimeException("Falha ao converter numero");
+        }
     }
 }
